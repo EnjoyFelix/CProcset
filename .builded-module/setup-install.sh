47c10c358444b96e40e78c6ad1a3771793273a6b
lgardon <lilian.gardon@inria.fr>
2023-05-26 14:34:57 +0200

    Init of the project

---
+++ .builded-module/setup-install.sh
@@ -0,0 +1,6 @@
+# run this script to install all the modules of the project. The created repository will be here.
+
+cd /home/lgardon/pset-cpython/.modules
+source ../.venv/bin/activate
+
+python ../setup.py install
