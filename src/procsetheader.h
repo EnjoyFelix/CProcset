392c64acce75855b4ea9a8a018ec637443edb124
Elisée Chemin <Elink.chem@gmail.com>
2024-04-23 10:35:21 +0200

    Added a header file for the structure definition

---
+++ src/procsetheader.h
@@ -0,0 +1,24 @@
+#ifndef PROCSET_HEADER_H_  /* convention classique: nom du module en majuscules, suivi de _H_ */
+#define PROCSET_HEADER_H_
+
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+typedef uint32_t pset_boundary_t;
+pset_boundary_t MAX_BOUND_VALUE = UINT32_MAX;
+
+// Definition of the ProcSet struct
+typedef struct {
+    //Python object boilerplate
+    PyObject_HEAD
+
+    //Boundaries of the ProcSet paired two by two as half-opened intervals
+    pset_boundary_t *_boundaries;   
+
+    //Number of boundaries, (2x nbr of intervals)
+    Py_ssize_t nb_boundary;
+} ProcSetObject;
+
+
+#endif
